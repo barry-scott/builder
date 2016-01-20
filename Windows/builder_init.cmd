99c42189df87257b34bdc066e8e9f113a3f388ac
Barry Scott <barry@barrys-emacs.org>
2016-01-20 16:42:58 +0000

    THis is improved version for windows.

--- Windows/builder_init.cmd
+++ Windows/builder_init.cmd
@@ -3,15 +3,16 @@ echo Info: Builder Init
 set BUILDER_START_DIR=%CD%
 rem search for builder custom init
 :scan
-    if exist Builder goto :found
+    if exist Builder goto found
     for %%I in (%CD%) do set __builder_tmp=%%~pI
-    if %__builder_tmp% == \ goto :not_found
+    if %__builder_tmp% == \ goto not_found
     cd ..
-    goto :scan
+    goto scan
 :found
 
-if not exist "Builder\builder_custom_init.cmd" goto :not_found
+if not exist "Builder\builder_custom_init.cmd" goto not_found
 
+set __builder_tmp=
 set BUILDER_TOP_DIR=%CD%
 
 cd Builder
@@ -20,4 +21,5 @@ call builder_custom_init.cmd
 
 :not_found
 cd %BUILDER_START_DIR%
+set __builder_tmp=
 echo on
