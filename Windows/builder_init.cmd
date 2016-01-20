cbb1d903101296544e5d6f8439220da16f150bcb
Barry Scott <barry@fender.chelsea.private>
2016-01-20 16:31:19 +0000

    Here are the versions of builder_init that I have been using on my personal projects for a while.

---
+++ Windows/builder_init.cmd
@@ -0,0 +1,23 @@
+@echo off
+echo Info: Builder Init
+set BUILDER_START_DIR=%CD%
+rem search for builder custom init
+:scan
+    if exist Builder goto :found
+    for %%I in (%CD%) do set __builder_tmp=%%~pI
+    if %__builder_tmp% == \ goto :not_found
+    cd ..
+    goto :scan
+:found
+
+if not exist "Builder\builder_custom_init.cmd" goto :not_found
+
+set BUILDER_TOP_DIR=%CD%
+
+cd Builder
+echo Info: Builder running builder_custom_init.cmd
+call builder_custom_init.cmd
+
+:not_found
+cd %BUILDER_START_DIR%
+echo on
